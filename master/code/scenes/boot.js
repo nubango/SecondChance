271717b4eea32ce744805e26adaa8d2128be4ab8
Marcos Docampo <marcosdo@ucm.es>
2019-11-17 10:29:46 +0100

    Started scene ZEN

--- master/code/scenes/boot.js
+++ master/code/scenes/boot.js
@@ -5,8 +5,13 @@ export default class Boot extends Phaser.Scene {
   preload() {
     // Hacer esto cuando haya cargado todo (callback)
     this.load.on("complete", () => { this.scene.start("MENU"); })
-    // Carga de imagenes
+    
+    // < Carga de imagenes >
+    // Background
     this.load.image("background", "./resources/images/background.jpg");
+    // Logo
+    this.load.image("logo", "./resources/images/web/OrigamiSlashBig.png");
+    // Frutas
     this.load.image("platano", "./resources/images/fruits/banana.png");
     this.load.image("moraA", "./resources/images/fruits/black-berry-dark.png");
     this.load.image("moraB", "./resources/images/fruits/black-berry-light.png");
@@ -15,11 +20,19 @@ export default class Boot extends Phaser.Scene {
     this.load.image("manzanaA", "./resources/images/fruits/green-apple.png");
     this.load.image("uvaA", "./resources/images/fruits/green-grape.png");
     this.load.image("limon", "./resources/images/fruits/lemon.png");
-    this.load.image("logo", "./resources/images/web/OrigamiSlashBig.png");
-  }
-
-  create() {
-    // Cuando cargue salta al menu
-    //this.scene.start("MENU");
+    this.load.image("lima", "./resources/images/fruits/lime.png");
+    this.load.image("naranja", "./resources/images/fruits/orange.png");
+    this.load.image("malocoton", "./resources/images/fruits/peach.png");
+    this.load.image("pera", "./resources/images/fruits/pear.png");
+    this.load.image("ciruela", "./resources/images/fruits/plum.png");
+    this.load.image("frambuesa", "./resources/images/fruits/raspberry.png");
+    this.load.image("manzanaB", "./resources/images/fruits/red-apple.png");
+    this.load.image("cerezaA", "./resources/images/fruits/red-cherry.png");
+    this.load.image("uvaB", "./resources/images/fruits/red-grape.png");
+    this.load.image("frutaRara", "./resources/images/fruits/star-fruit.png");
+    this.load.image("fresa", "./resources/images/fruits/strawberry.png");
+    this.load.image("sandia", "./resources/images/fruits/watermelon.png");
+    // Tinta
+    this.load.image("tinta", "./resources/images/punto-azulB.png");
   }
 }
--- master/code/scenes/zen.js
+++ master/code/scenes/zen.js
@@ -1,18 +1,18 @@
 import Fruit from "../objects/fruit.js"
 
 var colour = 60;
+var timerText;
+var timedEvent;
 
-export default class Arcades extends Phaser.Scene {
+export default class Zen extends Phaser.Scene {
     constructor() {
-        super({ key: "ARCADES", active: false });
+        super({ key: "ZEN" });
     }
 
     create() {
         // Centro x, y : Size w, h
         let width = this.sys.game.config.width;
         let heigth = this.sys.game.config.height;
-        let center_width = this.sys.game.config.width / 2;
-        let center_heigth = this.sys.game.config.height / 2;
 
         // Background con forma de cuaderno
         var background = this.add.image(0, 0, "background");
@@ -33,8 +33,9 @@ export default class Arcades extends Phaser.Scene {
             }
         }, this);
 
-        // Frutas
-        var frutilla = new Fruit(this, 123, 231, "moraA");
+        // 
+        timerText = this.add.text(width * 0.5, heigth * 0.1, "", { font: "72px adventpro", fill: "#222222" });
+        //timedEvent = this.time.addEvent({ delay: 500, callback: goMenu, callbackScope: this, loop: false });
     }
 
     update(time, delta) {
@@ -43,5 +44,11 @@ export default class Arcades extends Phaser.Scene {
         if (colour === 180) {
             colour = 60;
         }
+        // Actualiza el timer
+        //timerText.setText(timedEvent.getProgress().toString());
+    }
+
+    goMenu() {
+        this.scene.start("MENU");
     }
 }
